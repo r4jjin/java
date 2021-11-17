29ca2954a4fd5f6149cc2945a1f8af0470cec19d
Detoxic Spirit <r4jjin@gmail.com>
2021-11-17 14:14:30 +0530

    git init: sample java code to remove a char from a string

---
+++ coding_test/test1.java
@@ -0,0 +1,22 @@
+/*package whatever //do not write package name here */
+
+/**
+ * Sample code to remove a char from a string
+ */
+class test1 {
+	public static void main (String[] args) {
+		System.out.println("GfG!");
+		String input="Denny Mathew";
+		System.out.println(removeChars(input,'e'));
+	}
+	public static String removeChars(String input, char key){
+	    String output="";
+	    for(int i=0; i<input.length(); i++){
+	        char x=input.charAt(i);
+	        if(x!=key){
+	            output=output+x;
+	        }
+	    }
+	    return output;
+	}
+}
